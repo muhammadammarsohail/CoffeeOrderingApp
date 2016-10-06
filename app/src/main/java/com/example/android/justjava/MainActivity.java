9e72e6bcda8dc478590a58aab0b81e49f6e10551
muhammadammarsohail <muhammadammarsohail@gmail.com>
2016-10-06 10:50:47 +0500

    quantity initialized to 0

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -27,7 +27,7 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 2;
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
